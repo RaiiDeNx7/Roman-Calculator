25cc90ddb8b75d56f3e1e277f2067c82d256b352
Hunter Smith <huntertsmith4778@gmail.com>
2024-08-27 16:23:48 -0400

    Filled array in main with loops. Added new file which will determine value of roman numeral

---
+++ RomanNumeralValue.py
@@ -0,0 +1,23 @@
+def StringValue(letter):
+    match letter:
+        case "I" | "i" :
+            return "1"
+        case "V" | "v":
+            return "5"
+        case "X" | "x":
+            return "10"
+        case "L" | "l" :
+            return "50"
+        case "C" | "c":
+            return "100"
+        case "D" | "d":
+            return "500"
+        case "M" | "m":
+            return "1000"
+        case " ":
+            return " "
+
+
+        # If an exact match is not confirmed, this last case will be used if provided
+        case _:
+            return "Error"
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from mathfunction import *
+from RomanNumeralValue import *
 
-x = 5
-y = 10
-print(add(x,y))
+#Get input from user
+numeral = input("\nEnter your equation: ")
+print(len(numeral))
+
+#initialize array
+equation = []
+
+#Fill array
+for x in numeral:
+    equation.append(x)
+
+print(equation)
+
+i=0
+while i < len(equation):
+    equation[i] = StringValue(equation[i])
+    i += 1
+
+
+print(equation)
+
+
